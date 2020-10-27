39b4ea0b21f8d9fff919ee36ffc2b10056c95490
Erick Hoffmann <erickl.hoffmann@gmail.com>
2020-10-27 13:18:11 -0300

    Update add and remove order iten.

--- orderapp/src/main/java/com/order/orderapp/service/OrderItemService.java
+++ orderapp/src/main/java/com/order/orderapp/service/OrderItemService.java
@@ -4,6 +4,7 @@ import com.order.orderapp.model.ItemStatus;
 import com.order.orderapp.model.OrderItem;
 import com.order.orderapp.model.Product;
 import com.order.orderapp.repository.OrderItemRepository;
+import com.order.orderapp.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,9 @@ public class OrderItemService {
     @Autowired
     OrderItemRepository orderItemRepository;
 
+    @Autowired
+    OrderService orderService;
+
     public List<OrderItem> listOrderItems(long idOrder){
         return (List<OrderItem>) orderItemRepository.findAllOrderItems(idOrder);
     }
@@ -25,20 +29,29 @@ public class OrderItemService {
 
     public OrderItem insertUpdateOrderItem(OrderItem orderItem){
 
+        OrderItem orderItemSaved = null;
+
         if (orderItem.getItem() instanceof Product) {
             if(((Product)orderItem.getItem()).getItemStatus() == ItemStatus.ENABLE){
-                return orderItemRepository.save(orderItem);
+                orderItemSaved = orderItemRepository.save(orderItem);
             }
         }else{
 
-            return orderItemRepository.save(orderItem);
+            orderItemSaved =  orderItemRepository.save(orderItem);
         }
 
-        return null;
+        if(orderItemSaved != null){
+            orderService.insertUpdateOrder(orderItemSaved.getOrder());
+        }
+
+        return orderItemSaved;
     }
 
     public void deleteOrderItem(long id){
         OrderItem orderItemDelete = getOrderItem(id);
         orderItemRepository.delete(orderItemDelete);
+
+        orderItemDelete.getOrder().removeOrderItem(orderItemDelete);
+        orderService.insertUpdateOrder(orderItemDelete.getOrder());
     }
 }
